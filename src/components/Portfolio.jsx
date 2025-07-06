984c5ee5624651706ba7f3c15dd2ad3718d298ff
Jamel <jamelrablapro@gmail.Com>
2025-07-07 00:09:01 +0200

    maj lien linkedin

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -434,7 +434,7 @@ const Portfolio = () => {
                 Envoyer un email
               </a>
               <a 
-                href="https://linkedin.com"
+                href="https://www.linkedin.com/in/jamel-rabla/"
                 className="flex items-center justify-center px-8 py-4 bg-white/10 rounded-full hover:bg-white/20 transition-all duration-300 hover:scale-105"
                target="_blank">
                 <Linkedin className="w-5 h-5 mr-2" />
